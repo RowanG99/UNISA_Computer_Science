318ab4c78bd3a2afdf1501d02f7b59e2eb2900fb
Rowan Gauld <rowangauld1@gmail.com>
2023-07-05 21:47:19 +0200

    Chapter 6

--- FirstYear/SemesterOne/COS1512/Lessons/Chapter6/Chapter6MessingAround.cpp
+++ FirstYear/SemesterOne/COS1512/Lessons/Chapter6/Chapter6MessingAround.cpp
@@ -1,64 +1,54 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
+#include <iomanip>
 
 using namespace std;
+void makeNeat(ifstream &messyFile, ofstream &neatFile,int numberAfterDecimalpoint, int fieldWidth);
 
-int main()
+int main( )
 {
-//    ifstream inStream;
-//    ofstream outStream;
-//
-//    inStream.open("infile.dat");
-//    outStream.open("outfile.dat");
-//    int first, second, third;
-//    inStream >> first >> second >> third;
-//    outStream << "The sum of the first 3\n"
-//              << "numbers in infile.dat\n"
-//              << "is " << (first + second + third)
-//              << endl;
-//    inStream.close();
-//    outStream.close();
-
-    char inFileName[16], outFileName[16];
-    ifstream inStream;
-    ofstream outStream;
-
-    cout << "I will sum three numbers take from an input\n"
-         << "file and write the sum to an output file.\n";
-    cout << "Enter the input file name (max of 15 characters):\n";
-    cin >> inFileName;
-    cout << "Enter the output file name (max of 15 characters):\n";
-    cin >> outFileName;
-    cout << "I will read numbers from the file "
-         << inFileName << " and\n"
-         << "place the sum in the file "
-         << outFileName << endl;
-
-    inStream.open(inFileName);
-    if(inStream.fail())
+    ifstream fin;
+    ofstream fout;
+    fin.open("rawdata.dat");
+    if (fin.fail( ))
     {
         cout << "Input file opening failed.\n";
         exit(1);
     }
-    outStream.open(outFileName);
-    if(outStream.fail())
+    fout.open("neat.dat");
+    if (fout.fail( ))
     {
         cout << "Output file opening failed.\n";
         exit(1);
     }
-    int first, second, third;
 
-    inStream >> first >> second >> third;
-    outStream << "The sum of the first 3\n"
-              << "numbers in " << inFileName << endl
-              << "is " << (first + second + third)
-              << endl;
-
-    inStream.close();
-    outStream.close();
+    makeNeat(fin, fout, 5, 12);
+    fin.close( );
+    fout.close( );
 
     cout << "End of program.\n";
-
     return 0;
+}
+
+
+void makeNeat(ifstream &messyFile, ofstream &neatFile,
+              int numberAfterDecimalpoint, int fieldWidth)
+{
+    neatFile.setf(ios::fixed);
+    neatFile.setf(ios::showpoint);
+    neatFile.setf(ios::showpos);
+    neatFile.precision(numberAfterDecimalpoint);
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.setf(ios::showpos);
+    cout.precision(numberAfterDecimalpoint);
+
+    double next;
+    string whiteSpace = " ";
+    while (messyFile >> next || messyFile >> whiteSpace)
+    {
+        cout << setw(fieldWidth) << next << endl;
+        neatFile << setw(fieldWidth) << next << endl;
+    }
 }
